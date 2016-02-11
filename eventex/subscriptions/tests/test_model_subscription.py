a45235760222840006f836db89d44c16198ae66a
Frank Hermes Rocha Mendonça <frankmed57@gmail.com>
2016-02-10 22:17:31 -0200

    Persiste a inscricao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Frank Hermes',
+            cpf='12345678901',
+            email='frank@hermes.net',
+            phone='19-999999999'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_create_at(self):
+        """Subscription must have an auto created at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
